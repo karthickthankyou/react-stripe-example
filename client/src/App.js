ab067ac22d2b603c769260711d082f3cbba63cf8
Karthick <karthick@Karthicks-MacBook-Air.local>
2020-08-05 16:45:11 +0530

    Made cards lookable

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,113 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import StripeCheckout from "react-stripe-checkout"
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 
 import './App.css'
 
 function App() {
-  const [product] = useState({
-    name: 'Magic Pencil',
-    price: 2300,
-    description: 'This pencil brings drawn things to life.'
-  })
+  const [loading, setLoading] = useState(false)
+  const [products] = useState([
+    {
+      name: 'Magic Pencil',
+      price: 350,
+      description: 'This pencil brings drawn things to life.',
+      image: require('./asset/img/pencil.jpg')
+    },
+    {
+      name: 'Magic Cycle',
+      price: 52300,
+      description: 'This cycle will take you to the moon.',
+      image: require('./asset/img/cycle.jpg')
+    },
+    {
+      name: 'Magic Umbrella',
+      price: 2300,
+      description: 'This pencil brings drawn things to life.',
+      image: require('./asset/img/umbrella.jpg')
+    },
+    {
+      name: 'Magic Umbrella',
+      price: 2300,
+      description: 'This pencil brings drawn things to life.',
+      image: require('./asset/img/umbrella.jpg')
+    },
+  ])
 
-  const handleToken = async (token) => {
+  const handleToken = async (token, product) => {
     const response = await axios.post(process.env.REACT_APP_SERVER_CHECKOUT_URL, {
       token,
       product
     })
 
-    const { status } = response.data
-    if (status == 'success') {
+    const { success } = response.data
+    if (success) {
       console.log('Success', response.data)
+      // NotificationManager.info('Payment successful')
+      toast.success('🦄 Payment Successful')
     } else {
       console.log('Error', response.data)
+      toast.error('Payment Failed')
     }
   }
 
+  const copyCardNumber = (phoneNumber) => {
+    toast.info(`Card number ${phoneNumber} copied.`)
+  }
+
   return (
-    <div className="App">
-      Magic Pencil
-      <StripeCheckout
-        stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
-        token={handleToken}
-        amount={product.price * 100}
-        name={product.name}
-        billingAddress
-        shippingAddress
-      />
+
+
+    <div>
+      <div className="container">
+        {
+          products.map(product => (
+            <div className="card">
+              <img src={product.image} alt="Avatar" style={{ width: "100%" }} />
+              <h4><b>{product.name}</b></h4>
+              <p>${product.price / 100}</p>
+
+              <StripeCheckout
+                stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
+                token={(token) => handleToken(token, product)}
+                amount={product.price}
+                name={product.name}
+                billingAddress
+                shippingAddress
+              />
+            </div>)
+          )
+        }
+        <ToastContainer position="top-right" autoClose={3000} />
+
+
+      </div>
+      <div className="card_hint">
+        <h1>Card Numbers</h1>
+        <CopyToClipboard text="4242424242424242"
+          onCopy={() => toast.info(`Card number 4242424242424242 copied.`)}>
+          <button>Copy valid card number</button>
+        </CopyToClipboard>
+        <CopyToClipboard text="4000008260003178"
+          onCopy={() => toast.info(`Card number 4000008260003178 copied.`)}>
+          <button>Copy invalid card number</button>
+        </CopyToClipboard>
+
+
+
+
+
+      </div>
     </div>
   )
 }
 
 export default App
+
+
+
--- server.js
+++ server.js
@@ -1,13 +1,60 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+// TODO: Add stripe key
 
+const { STRIPE_KEY } = process.env
+const stripe = require('stripe')(STRIPE_KEY)
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
+app.use(express.json())
+app.use(cors())
+
 app.get('/', (req, res) => {
-    res.json("Hello World")
+    res.send('Stripe Example')
 })
 
-app.listen(7002, () => {
-    console.log('App running on PORT 7002')
+app.post('/checkout', async (req, res) => {
+    try {
+        const { product, token } = req.body
+        console.log(product)
+        const customer = await stripe.customers.create({
+            email: token.email,
+            source: token.id
+        })
+
+        const idempotency_key = uuidv4()
+        const charge = await stripe.charges.create({
+            amount: product.price,
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Purchased the ${product.name}`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    line1: token.card.address_line1,
+                    line2: token.card.address_line2,
+                    city: token.card.address_city,
+                    country: token.card.address_country,
+                    postal_code: token.card.address_zip
+                }
+            }
+        },
+            {
+                idempotency_key
+            }
+        )
+        res.json({ success: true, charge })
+        console.log(charge)
+    } catch (err) {
+        console.log(err)
+        res.json({ success: false, err })
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App listening at ${process.env.PORT}`)
 })
